5781f04e3b2c06af5cc5c7753409b2a089f35d2c
daxplrer <70825723+daxplrer@users.noreply.github.com>
2024-05-22 10:03:58 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#beberapa pake bitly soalnya link rawnya suka berubah setiap ngedit, klo selain raw gbs diautomate di terminal
+#ini dari: https://gist.github.com/daxplrer/cb24587d86a1ac342991dc36f67c33f6
+wget https://bit.ly/pipillgiver_setupv3 && sh pipillgiver.sh
+sudo pip install playsound3 pyTelegramBotAPI --break-system-packages
