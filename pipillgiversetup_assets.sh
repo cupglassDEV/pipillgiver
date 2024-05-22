41f09eccef09e05000861dca8c8a39c21b605b8b
daxplrer <70825723+daxplrer@users.noreply.github.com>
2024-05-22 11:18:05 +0000

    Update pipillgiversetup_assets.sh

--- pipillgiversetup_assets.sh
+++ pipillgiversetup_assets.sh
@@ -3,7 +3,7 @@ sudo mkdir pipillgiver_assets
 cd ~/pipillgiver_assets
 echo 'Opening' |   sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file opening_narrator.wav
 echo 'Have a nice day, sir!' |   sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file h4vaniced8mr_narrator.wav
-echo 'Hello mr' |    sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file hello_narrator.wav
+echo 'Hello sir' |    sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file hello_narrator.wav
 echo 'Okay' |   sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file ok_narrator.wav
 echo 'Sending to:' | sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file sendingto_narrator.wav
 echo 'Telegram' | sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file c_tg_narrator.wav
