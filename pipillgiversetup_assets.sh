bca986bf5bcece04de21d06b0ca12ff690122ae6
daxplrer <70825723+daxplrer@users.noreply.github.com>
2024-05-25 13:16:36 +0000

    Update pipillgiversetup_assets.sh

--- pipillgiversetup_assets.sh
+++ pipillgiversetup_assets.sh
@@ -1,11 +1,13 @@
-cd ~/
+USR=$(id -u -n)
+cd /home/$USR/
 sudo mkdir pipillgiver_assets
-cd ~/pipillgiver_assets
+cd /home/$USR/pipillgiver_assets
 echo 'Opening' |   sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file opening_narrator.wav
-echo 'Have a nice day, sir!' |   sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file h4vaniced8mr_narrator.wav
+echo 'Have a nice day, sir!' |   sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file h4vaniced8sir_narrator.wav
 echo 'Hello sir' |    sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file hello_narrator.wav
+echo 'From the scheduled time in Telegram' |    sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file schtme_tg_narrator.wav
 echo 'Okay' |   sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file ok_narrator.wav
 echo 'Sending to:' | sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file sendingto_narrator.wav
 echo 'Telegram' | sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file c_tg_narrator.wav
 echo 'You are receiving a text from:' | sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file txtreceived_msg_narrator.wav
-echo 'Heres the pill' | sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file opened_narrator.wav
+echo 'Heres your pill' | sudo piper --model ../piper_models/en_US-hfc_male-medium.onnx --output_file opened_narrator.wav
